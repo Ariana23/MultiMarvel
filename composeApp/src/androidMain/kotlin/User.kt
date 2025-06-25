c1caf9b82529e3749cd923f5a1becef13595d386
Ariana23 <arianavargas235@hotmail.com>
2025-06-25 17:19:34 -0300

    cositas del front

--- composeApp/src/androidMain/kotlin/User.kt
+++ composeApp/src/androidMain/kotlin/User.kt
@@ -1,32 +1,41 @@
 package ui
 
-import android.util.Log
+import androidx.compose.foundation.layout.Box
 import org.multimarvel.R
 
 //import androidx.compose.animation.core.R
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-
-
-import androidx.compose.ui.res.painterResource
-
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+
 
 import androidx.compose.ui.unit.dp
 
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CharacterListScreen(viewModel: CharactersViewModel) {
     val characters by viewModel.characters.collectAsState()
@@ -34,34 +43,59 @@ fun CharacterListScreen(viewModel: CharactersViewModel) {
     LaunchedEffect(Unit) {
         viewModel.fetchCharacters()
     }
-
-    LazyColumn {
-        items(characters) { character ->
-
-            val imageUrl = character.thumbnail.path.replace("http://", "https://") +
-                    "/portrait_xlarge." + character.thumbnail.extension
-
-            Spacer(Modifier.height(3.dp))
-            Column(
-                modifier = Modifier.padding(10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    modifier = Modifier.padding(5.dp),
-                    text = "${character.name}: ${character.description}"
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                        Text(
+                            text = "Multi Marvel App",
+                            style = MaterialTheme.typography.titleLarge,
+                            color = Color.White
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(138, 0, 0)
                 )
+            )
+        }
+    ) { innerPadding ->
 
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data(imageUrl)
-                        .crossfade(true)
-                        .error(R.drawable.meme)
-                        .build(),
-                    contentDescription = character.name,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(200.dp)
-                )
+        LazyColumn (
+            contentPadding = PaddingValues(
+                top = innerPadding.calculateTopPadding() + 8.dp,
+                bottom = 16.dp,
+                start = 16.dp,
+                end = 16.dp
+        )){
+            items(characters) { character ->
+
+                val imageUrl = character.thumbnail.path.replace("http://", "https://") +
+                        "/portrait_xlarge." + character.thumbnail.extension
+
+                Spacer(Modifier.height(3.dp))
+                Column(
+                    modifier = Modifier.padding(10.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        modifier = Modifier.padding(5.dp),
+                        text = "${character.name}: ${character.description}"
+                    )
+
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(imageUrl)
+                            .crossfade(true)
+                            .error(R.drawable.meme)
+                            .build(),
+                        contentDescription = character.name,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                    )
+                }
             }
         }
     }
